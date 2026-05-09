dae65a1c4224bb26c2dddc63f21beadc3568aca5
boubou-coach <mathilde.aimard34@gmail.com>
2026-05-09 22:15:41 +0200

    mise à jour2

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { registerSW } from 'virtual:pwa-register'
+import ReactGA from "react-ga4";
 import App from './App.tsx'
 import './index.css'
 
 registerSW({ immediate: true })
+
+ReactGA.initialize("G-4N75KCDMQX");
+ReactGA.send("pageview");
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>

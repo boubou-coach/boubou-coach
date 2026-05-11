a9bffca425df1226d5ebc7f9df586ebbe8b0873b
boubou-coach <mathilde.aimard34@gmail.com>
2026-05-11 22:27:50 +0200

    bouton maj

--- src/App.tsx
+++ src/App.tsx
@@ -1634,7 +1634,7 @@ export default function App() {
   }}
   className="mt-4 rounded-2xl bg-[#101827] px-5 py-3 text-sm font-black text-white"
 >
-  Installer l’application
+  Installer l’application 
 </button>
               <h1 className="text-xl font-black tracking-tight lg:text-3xl">
                 Dressage bouledogue français
